69d6e1d80fa3b77c37e207c3b19b9b3908b4e286
huangqun <huangqun@finupgroup.com>
2017-12-29 21:57:19 +0800

    no message

--- crawler/config/dbConfig.js
+++ crawler/config/dbConfig.js
@@ -4,7 +4,7 @@
 const Sequelize = require('sequelize');
 
 // 使用URI形式连接
-const moneyDB = new Sequelize('mysql://username:puhui@10.10.232.242:3306/holmes', {
+const moneyDB = new Sequelize('mysql://admin_gao:puhui@10.10.232.242:3306/holmes', {
   define: {
     timestamps: false // 取消Sequelzie自动给数据表加入时间戳（created以及updated）
   }
